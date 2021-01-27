ee0b0579b6492187d52a7a30f1c4a02f9d4ef8eb
Christian Ontiveros <contiverosv@outlook.com>
2021-01-27 17:58:45 -0600

    Initial commit

---
+++ src/ejemploparaserundiosprogramando/EjemploParaSerUnDiosProgramando.java
@@ -0,0 +1,69 @@
+
+package ejemploparaserundiosprogramando;
+
+public class EjemploParaSerUnDiosProgramando {
+
+    public static void main(String[] args) {
+        
+        //String cadenaUno = "Adios Mundo", cadenaDos = "Adios Mexico";
+        //IF switch While Do While For
+        //Compuerta OR ||
+        //Compuerta AND &&
+        /*OR
+            T T  T
+            T F  T
+            F T  T
+            F F  F
+        
+            AND
+            T T  T
+            T F  F
+            F T  F
+            F F  F
+        
+            = asignar :D
+            == comparar
+            != distinto de
+        
+        
+        int Numero = 1;
+        
+        if(Numero != 1){
+            
+        }else{
+            
+        }
+        Ejemplo IF :D
+        if(cadenaUno.equalsIgnoreCase(cadenaDos) && cadenaDos.equalsIgnoreCase("Adios Mexico")){
+            System.out.println("Hola Mundo");   
+        }else if(1==1){
+            System.out.println("Oye bro, no se cumplio tu condición, por lo tanto estoy aquí uwu");
+        }else{
+            
+        }
+        Operador Ternario
+        String Hola = (1 == 1)?"Adios Mundo":"Hola Mundo";
+        
+        if(1 == 1){
+            Hola = "Adios Mundo";
+        }else{
+            Hola = "Hola Mundo";
+        }
+        
+        while(1 == 2){
+            System.out.println("Soy un bucle :D");
+        }
+        
+        do{
+            System.out.println("Soy un bucle :D");
+        }while(1 == 2);
+        
+        for(int i = 0;i < 28; System.out.println("El numero i es igual a " + i),i++);
+        
+        */
+        
+        
+        
+    }
+    
+}
